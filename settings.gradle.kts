79d9b115d417a58e19bae2c3ab5a1996e0c02078
Sebastien Russell <sebastien.russell@desjardins.com>
2023-12-01 13:29:01 -0500

    Fix project on my computer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,11 @@ rootProject.name = "advent-of-code-2023"
 
 dependencyResolutionManagement {
     repositories {
+        mavenLocal()
         mavenCentral()
+        google()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
     }
 }
