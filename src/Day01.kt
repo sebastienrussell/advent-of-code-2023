f0f31b04e149ff6f2b511852eb894841f69ecbdf
Sebastien Russell <sebastien.russell@desjardins.com>
2023-12-01 15:16:31 -0500

    day one part2 two took me so much time omg ...

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,61 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+            input.map { line ->
+                val eachLineNumbers = line.filter { it in '0'..'9' }
+                println(line to "${eachLineNumbers.first()}${eachLineNumbers.last()}".toInt())
+                line to "${eachLineNumbers.first()}${eachLineNumbers.last()}".toInt()
+            }.sumOf { it.second }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+
+    fun part2(input: List<String>): Int =
+            input.map { line ->
+                val eachLineNumbers = line.replaceFirstAndLastWrittenDigits().filter { it in '1'..'9' }
+                println(line to "${eachLineNumbers.first()}${eachLineNumbers.last()}".toInt())
+                line to "${eachLineNumbers.first()}${eachLineNumbers.last()}".toInt()
+            }.sumOf { it.second }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
+    check(part1(input) == 54159)
+    val testInputPart2 = readInput("Day02_test")
+    check(part2(testInputPart2) == 281)
+//    check(part2(input) !in listOf(53869, 53880)) // Not the rights answers
     part2(input).println()
+    "eighthree".replaceFirstAndLastWrittenDigits()
 }
+
+private val writtenDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+private fun String.replaceFirstAndLastWrittenDigits(list: List<String> = writtenDigits): String {
+    var filteredString = this
+    val firstNumber = filteredString.findAnyOf(list, ignoreCase = true)
+    val lastNumber = filteredString.findLastAnyOf(list, ignoreCase = true)
+
+    filteredString = firstNumber?.second.orEmpty().let { first ->
+        filteredString.replaceFirst(first, first.fromWrittenDigit(), true)
+    }
+
+    filteredString = lastNumber?.second.orEmpty().let { last ->
+        filteredString.reversed().replaceFirst(last.reversed(), last.fromWrittenDigit(), true).reversed()
+    }
+
+    return filteredString
+}
+
+private fun String.fromWrittenDigit(): String =
+        when (this) {
+            "one" -> "o1e"
+            "two" -> "t2o"
+            "three" -> "t3e"
+            "four" -> "f4r"
+            "five" -> "f5e"
+            "six" -> "s6x"
+            "seven" -> "s7n"
+            "eight" -> "e8t"
+            "nine" -> "n9e"
+            else -> ""
+        }
